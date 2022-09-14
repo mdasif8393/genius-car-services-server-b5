00d9cb0171dfb886ce7783e9bd0b5f0a8bf4c733
mdasif8393 <md.asif.8393@gmail.com>
2022-09-14 19:31:44 +0600

    add create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const port = process.env.port || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 
@@ -15,6 +15,35 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const database = client.db("geniusCar");
+    const serviceCollection = database.collection("service");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.json(services);
+
+      app.get("/service/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.json(service);
+      });
+
+      app.post("/service", async (req, res) => {
+        const newService = req.body;
+        const result = await serviceCollection.insertOne(newService);
+        res.json(result);
+      });
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running port 5000");
