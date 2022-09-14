9bb92098c9b27832b587c73ea091adce1a3de9e6
mdasif8393 <md.asif.8393@gmail.com>
2022-09-14 17:10:31 +0600

    add database and add dotenv

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const port = process.env.port || 5000;
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
+
+//middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h5pr3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("Running port 5000");
